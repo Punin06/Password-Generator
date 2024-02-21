29c34153c9d563caff44aa45a30c87f3adb7d9f2
Punin06 <90221164+Punin06@users.noreply.github.com>
2024-02-21 06:58:57 -0500

    Update password generator 5-02-24 Project.py

--- password generator 5-02-24 Project.py
+++ password generator 5-02-24 Project.py
@@ -21,6 +21,7 @@ def spc(password):
     password += special[x]
     return password
 
+#this func returns the password after adding a number character at random
 def numc(password):
     num = "0123456789"
     x = rand.randint(0,len(num)-1)
@@ -28,10 +29,16 @@ def numc(password):
     return password
 
 def main():
+    #Creating size to store the size of the password, password to store password, left to keep track of the missing criteria characters.
     size = int(input("Enter size of password, recommended size 12 +, 8+ is the minimum \n"))
     password = ""
     left = ""
- 
+
+    #input validation, to ensure the minimum strong password length is selected
+    while (size < 8):
+        size = int(input("The size of the password was less than the recommended! Please select a recommended size! Recommended size 12 +, 8+ is the minimum \n"))
+
+    #A loop to go to random func to return a random character such as upper, lower, special and number, and keeping track of the func visit with left.
     for i in range(size):
        x = rand.randint(1,4)
        if x==1:
@@ -49,17 +56,19 @@ def main():
        else:
             password = numc(password)
             left+=str(x)
-    #
+    #stores the responding choices selections
     m = "1234"
     
 
-
+#this goes through the password to check the missing criteria, and then fills it.
     for j in range (len(password)):
-        print(left)
         for i in range(len(m)):
+            #temp stores the missing criteria
             temp = left.rfind((m[i]))
-            
+
+            #if there's a miss it's, stored as -1. And a match is stored as 1, so we go through the missing criteria fill them and replace them with 1, to ensure that criteria is not visited again unless it's missing.
             if(temp == -1):
+                #creating re to go to a random point in the password and change it 
                 re = rand.randint(0, len(password)-1)
                 if int(m[i]) == 1 :
                     password = password.replace(password[re],lowc(""))
@@ -74,8 +83,9 @@ def main():
                 elif int(m[i]) == 4:
                     password = password.replace(password[re],numc(""))
                     left = left.replace(left[re],"4",1) 
+                #setting j to zero, to go through the password again, to ensure no criteria is missing.
                 j = 0
-              
+    #prints the password     
     print(password)  
 
 if __name__ == "__main__":
